7d4e90985ad57bbf9f898531a6e18c26881db7bc
DavidtpDEV <davidtp.dev@gmail.com>
2024-01-30 12:15:55 -0300

    Adicionando novo projeto CadastroCPF

---
+++ CadastroCPF/CadastroCPF/CPF.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CadastroCPF
+{
+    public class CPF
+    {
+        public static bool ValidarCPF(string cpf)
+        {
+            if (string.IsNullOrWhiteSpace(cpf))
+                return false;
+
+            cpf = cpf.Replace(".", "").Replace("-", "");
+
+            if (cpf.Length != 11)
+                return false;
+
+            if (!long.TryParse(cpf, out _))
+                return false;
+
+            if (cpf == "00000000000" ||
+                cpf == "11111111111" ||
+                cpf == "22222222222" ||
+                cpf == "33333333333" ||
+                cpf == "44444444444" ||
+                cpf == "55555555555" ||
+                cpf == "66666666666" ||
+                cpf == "77777777777" ||
+                cpf == "88888888888" ||
+                cpf == "99999999999")
+                return false;
+
+            int[] multiplier1 = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplier2 = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            string tempCpf = cpf.Substring(0, 9);
+            int sum = 0;
+
+            for (int i = 0; i < 9; i++)
+                sum += int.Parse(tempCpf[i].ToString()) * multiplier1[i];
+
+            int remainder = sum % 11;
+            if (remainder < 2)
+                remainder = 0;
+            else
+                remainder = 11 - remainder;
+
+            string digit = remainder.ToString();
+            tempCpf += digit;
+
+            sum = 0;
+            for (int i = 0; i < 10; i++)
+                sum += int.Parse(tempCpf[i].ToString()) * multiplier2[i];
+
+            remainder = sum % 11;
+            if (remainder < 2)
+                remainder = 0;
+            else
+                remainder = 11 - remainder;
+
+            digit += remainder.ToString();
+
+            return cpf.EndsWith(digit);
+        }
+    }
+}
---
+++ CadastroCPF/CadastroCPF/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CadastroCPF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um CPF para validar:");
+            string cpf = Console.ReadLine();
+
+            if (CPF.ValidarCPF(cpf))
+                Console.WriteLine("CPF válido!");
+            else
+                Console.WriteLine("CPF inválido!");
+
+            Console.ReadLine();
+        }
+    }
+}
